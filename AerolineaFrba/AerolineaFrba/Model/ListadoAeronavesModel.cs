b0cbac6b034af427f745e559dfd216428e0cdf25
Guillermo Grillo <guillermo.grillo88@gmail.com>
2015-12-02 01:17:39 -0300

    a comerla

---
+++ AerolineaFrba/AerolineaFrba/Model/ListadoAeronavesModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba.Model
+{
+    public class ListadoAeronavesModel
+    {
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+
+        public ListadoAeronavesModel()
+        {
+
+        }
+
+        public ListadoAeronavesModel(string nombre, int cantidad)
+        {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+        }
+    }
+}
